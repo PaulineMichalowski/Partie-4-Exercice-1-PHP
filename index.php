84232f87b8adb15c8a910e4e65ed4e567c9abde2
Pauline Michalowski <michalowski.pauline@hotmail.fr>
2017-12-13 15:25:12 +0100

    Création de l'exercice 1

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Exercice 1</title>
+</head>
+<body>
+  <?php
+  function truly() {
+    return true;
+  }
+  echo truly();
+  ?>
+</body>
+</html>
